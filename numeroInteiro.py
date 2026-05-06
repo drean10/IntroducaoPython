40a379a7874587875eb13a3760b22064d57d8eda
drean10 <drean.luiz2008@gmail.com>
2026-05-06 00:02:48 -0300

    Trabalhando com numeros e strings

--- numeroInteiro.py
+++ numeroInteiro.py
@@ -1,3 +1,9 @@
+inteiro =  17 // 3
+print ( inteiro ) # 5
+# Acessando caracteres de uma string
 palavra = 'Python'
 palavra [ 0 ] # caractere na posição 0 'P' 
 palavra [ 5 ] # caractere na posição 5 ' n ' 
+print ( palavra [ 0 ] )
+print ( palavra [ 5 ] )
+print ( len ( palavra ) ) # 6
