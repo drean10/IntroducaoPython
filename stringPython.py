dd945aacd61b1d3113aa7676ac4c698b543d00c4
drean10 <drean.luiz2008@gmail.com>
2026-05-06 00:20:50 -0300

    trabalhando string no python

---
+++ stringPython.py
@@ -0,0 +1,7 @@
+# Acessando caracteres de uma string
+palavra = 'Python'
+palavra [ 0 ] # caractere na posição 0 'P' 
+palavra [ 5 ] # caractere na posição 5 ' n ' 
+print ( palavra [ 0 ] )
+print ( palavra [ 5 ] )
+print ( len ( palavra ) ) # 6
