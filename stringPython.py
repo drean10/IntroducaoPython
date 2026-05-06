3244bd22cbdc6405dcee19f45460ba0fcf6c0aae
drean10 <drean.luiz2008@gmail.com>
2026-05-06 00:41:02 -0300

    strings

--- stringPython.py
+++ stringPython.py
@@ -2,6 +2,23 @@
 palavra = 'Python'
 palavra [ 0 ] # caractere na posição 0 'P' 
 palavra [ 5 ] # caractere na posição 5 ' n ' 
+palavra [ -5 ] # caractere na posição -5 'y'
+palavra [ 2 : 5 ] # caracteres da posição 2 (incluído) à 5 (excluído) 'tho'
+palavra = 'J'  + palavra [ 1 : ]
 print ( palavra [ 0 ] )
 print ( palavra [ 5 ] )
-print ( len ( palavra ) ) # 6
+print ( palavra [ -5 ] )
+print ( palavra [ 2 : 5 ] )
+
+var = 3  *  'un'  +  'ium'  "\n" 'Py'  'thon' 
+print ( var ) # 'unununium'
+
+palavra = 'Python'
+var = 'J'  + palavra [ 1 : ]
+print ( var ) # 'Jython'
+
+palavra[ : 2 ]  +  'py' 
+print ( palavra) # 'Pypthon'
+
+s =  'supercalifragilisticexpialidocious' 
+print ( len ( s ) ) # 34
