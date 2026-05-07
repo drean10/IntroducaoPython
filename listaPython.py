6e4aa32bbed944852a10e360cc9e15c48de0ab05
drean10 <drean.luiz2008@gmail.com>
2026-05-06 23:55:18 -0300

    lista Python

---
+++ listaPython.py
@@ -0,0 +1,5 @@
+quadrados = [ 1 , 4 , 9 , 16 , 25 ] + [ 30, 35, 40]
+quadrados + [30,35,40] # [1, 4, 9, 16, 25, 30, 35, 40]
+print ( quadrados )
+print ( quadrados [ 0 ] ) # 1
+print ( quadrados [ 4 ] ) # 25
