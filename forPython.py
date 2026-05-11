cf6253bd64acce24127f24c50fe87aac0818361d
drean10 <drean.luiz2008@gmail.com>
2026-05-11 10:53:32 -0300

    Criação de uma função e chamada em outro arquivo

--- forPython.py
+++ forPython.py
@@ -1,4 +1,14 @@
-# Example: Nested for loops to print coordinates
-for i in range(3):
-    for j in range(2):
-        print(f"({i}, {j})")
+# Exemplo de duas formas de importar a função quadrado do arquivo funcaoPython.py e 
+# usá-la para calcular o quadrado dos números de 0 a 9 usando um loop for.
+
+from funcaoPython import *
+
+for i in range(10):
+    print(f"O quadrado de {i} é {quadrado(i)}")
+    
+
+#outra forma de inportar a função quadrado do arquivo funcaoPython.py
+import funcaoPython
+
+for i in range(10):
+    print(f"O quadrado de {i} é {funcaoPython.quadrado(i)}")
---
+++ funcaoPython.py
@@ -0,0 +1,11 @@
+#funções em python são blocos de código reutilizáveis que realizam uma tarefa específica. 
+# Elas ajudam a organizar o código, melhorar a legibilidade e evitar a repetição. 
+# Você pode definir suas próprias funções usando a palavra-chave def, seguida pelo nome da função e parênteses.
+#Exemplo de função simples
+def quadrado(x):
+    return x * x    
+
+#vamos usar a função quadrado para calcular o quadrado dos números de 0 a 9
+#vou usar outro arqivo para mostrar o uso da função quadrado, pois o arquivo forPython.py tem um exemplo de loop for e não de função.
+""" for i in range(10):
+    print(f"O quadrado de {i} é {quadrado(i)}") """
