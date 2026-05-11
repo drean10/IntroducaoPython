adafcebe2626d785e7ecaf31c38e71e697d176ce
drean10 <drean.luiz2008@gmail.com>
2026-05-11 09:49:18 -0300

    Loop em Python

---
+++ loopPython.py
@@ -0,0 +1,15 @@
+#range(6) para imprimir os números de 0 a 5
+for i in range(6):
+    print(i)
+
+#Mesma coisa que:
+""" for j in [0,1,2,3,4,5]:
+    print(j) """
+
+nomes = ["Alice", "Bob", "Charlie"]
+for nome in nomes:
+    print(nome)
+    
+nome = "Alice"
+for letra in nome:
+    print(letra)
